fe1ff7d4de776691912ef1ae983258c00473d211
devbn3li <fudwhc@gmail.com>
2024-01-25 19:10:52 +0200

    updates

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 function toggleAnswer(id) {
   let answer = document.getElementById("plus_" + id);
+  let box = document.getElementById("box" + id);
+  const list = box.classList;
+  let image = document.getElementById("image" + id);
+
   if (answer.style.display === "none") {
     answer.style.display = "block";
-    answer.style.marginTop = "10px";
-  } else {
+  } else if (answer.style.display === "block"){
     answer.style.display = "none";
   }
+  if (list.contains("box")) {
+    box.classList.remove("box");
+    box.classList.add("rm-box");
+    image.src = "./images/pluscircle.svg";
+  } else if (list.contains("rm-box")) {
+    box.classList.remove("rm-box");
+    box.classList.add("box");
+    image.src = "./images/minuscircle.svg";
+  }
+
 }
