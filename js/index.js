91ca6d728426336d16e4bfc4669e4da0cec52d4f
devbn3li <fudwhc@gmail.com>
2024-02-15 07:29:31 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+let box = document.getElementsByClassName("box");
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.maxHeight){
+      content.style.paddingTop = "0px"
       content.style.maxHeight = null;
+      box.style.border = "none";
     } else {
+      content.style.paddingTop = "10px"
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
 }
